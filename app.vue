e95bde1100fc700badd0d345dba98096a69e18ae
petedavisdev <hello@petedavis.dev>
2025-02-19 15:27:27 +0000

    typecheck, lint and format

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@
 		<nav>
 			<ul>
 				<li><NuxtLink to="/">Home</NuxtLink></li>
+			</ul>
+			<ul>
+				<li><NuxtLink to="/page1">Page 1</NuxtLink></li>
 				<li><NuxtLink to="/page2">Page 2</NuxtLink></li>
 			</ul>
 		</nav>
