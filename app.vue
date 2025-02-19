84a9185bb64a9888334156df4dff9c89480eccd3
petedavisdev <hello@petedavis.dev>
2025-02-19 15:15:44 +0000

    initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+	<header class="container">
+		<nav>
+			<ul>
+				<li><NuxtLink to="/">Home</NuxtLink></li>
+				<li><NuxtLink to="/page2">Page 2</NuxtLink></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main class="container">
+		<NuxtPage />
+	</main>
+
+	<NuxtRouteAnnouncer />
+</template>
+
+<style>
+@import url('https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css');
+</style>
